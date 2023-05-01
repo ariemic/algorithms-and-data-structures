85c00bbc356f5e7a411cdd60271d21a46ab5e1fa
Ariel <ariemichalik@gmail.com>
2023-05-01 12:30:07 +0200

    add changes

--- Graph_algo_ćwiczenia/14_dijkstra_adjancency_matrix.py
+++ Graph_algo_ćwiczenia/14_dijkstra_adjancency_matrix.py
@@ -1,40 +1,37 @@
 from queue import PriorityQueue
 
-def relax(u, v, edge_weight, d, parent):
-    if d[v] > d[u] + edge_weight:
+
+def relax(u, v, edge_weight, parent, d):
+    if d[v] > d[u]+edge_weight:
         d[v] = d[u] + edge_weight
         parent[v] = u
         return True
     return False
 
-def dijkstra(G, s, t):
+
+def Dijkstra(G, s, t):
     n = len(G)
-    d = [float('inf')]*n
-    parent = [None]*n
-    visited = [False]*n
-    
     q = PriorityQueue()
-    d[s] = 0
-    q.put((d[s], s))
+    d = [float('inf')]*n
+    visited = [False]*n
+    parent = [None]*n
+    d[s[0]] = 0
+    q.put((0, s[0]))
     
     while not q.empty():
-        d_u, u = q.get()
+        du, u = q.get()
         for v in range(n):
-            #co robi warunke G[u][v] ? - jeśli krawędz istnieje to jej waga jest nie ujemna
-            if not visited[v] and G[u][v] != -1 and relax(u, v, G[u][v], d, parent):
+            if not visited[v] and G[u][v] != -1 and relax(u, v, G[u][v], parent, d):
                 q.put((d[v], v))
-        visited[u] = True
-        if t == (u, v):
-            return d[t[0]]
+        visited[u] = True #zapobiega cofaniu się w tył (takie cofanie wydłuża ścieżke) ten wierzchołek jest już przetworzony
+        if t == (u,v):
+            return d[u]
         
-    # return d, parent
+G = [[-1, 7, 5, 2, -1, -1],
+     [7, -1, -1, -1, 3, 8 ],
+     [5, -1, -1, 10, 4, -1],
+     [2, -1, 10, -1, -1, 2], 
+     [-1, 3, 4, -1, -1, 6],
+     [-1, 8, -1, 2, 6, -1]]
 
-
-graph = [[-1, 3, 1, 2],
-         [3, -1, -1, -1], 
-         [1, -1, -1, 0],
-         [2, -1, 0, -1]]
-
-print(dijkstra(graph, 0, (3, 3)))
-# print(parent, distance)
-# [None, 0, 1, 2, 3] [0, 1, 3, 6, 7]
+print(Dijkstra(G, (0, 0), (5, 5)))
