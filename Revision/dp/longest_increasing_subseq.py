779872de2371807d2469bf06fc31ced577338043
Ariel <arie.michalik@gmail.com>
2024-09-21 13:09:32 +0200

    longest increasing subseq

---
+++ Revision/dp/longest_increasing_subseq.py
@@ -0,0 +1,38 @@
+""" 
+1. f(k) - dl najdłuższego podciągu kończącego się na A[k]
+2. f(k) = max {f(t) + 1 | t < k ^ A[t] < A[k]}
+"""
+def longest_not_coherent_subseq(A):
+  n = len(A)
+  F = [1]*n
+  Parent = [-1]*n
+  for k in range(1, n):
+    # nie możemy A[k] porównywać tylko z A[k-1] bo nie wiemy ile wczesniej jest liczba mniejsza od A[k] a nie możemy jej (ich ) ominąć
+    # co prawda jeśli dojdzemy do pierwszej mniejszej idąc od tyłu to F[t] juz nie bedzie pozniej wieksze wiec moglibysmy na tym skonczyc
+    for t in range(k):
+      if A[t] < A[k] and F[t] < F[k] + 1:
+        F[k] = F[t] + 1
+        Parent[k] = t
+  return F[-1]
+
+
+
+#czy ta wersja zawsze zadziała ? Nie mam pewności
+def lis(A):
+  n = len(A)
+  F = [1]*n
+  for k in range(n):
+    for t in range(k, -1, -1):
+      if A[t] < A[k] and F[k] < F[t] + 1:
+        F[k] = F[t] + 1
+        break
+  return F[-1]
+    
+
+A = [2, 1, 4, 3, 4, 8, 5, 7]
+A2 = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+
+# print(longest_not_coherent_subseq(A))
+# print(lis(A))
+print(longest_not_coherent_subseq(A2))
+print(lis(A2))
